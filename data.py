5e54ec0407bfa7a009f821857b1ecd9435d97915
Diego Alberto López Lazareno <if722100@iteso.mx>
2022-03-15 11:54:44 -0600

    Configuraciones iniciales

--- data.py
+++ data.py
@@ -1,6 +1,6 @@
 """
 # -- --------------------------------------------------------------------------------------------------- -- #
-# -- project: Lab 1.                                                                                     -- #
+# -- project: Lab 3.                                                                                     -- #
 # -- script: data.py : python script for data collection                                                 -- #
 # -- author: diegolazareno                                                                               -- #
 # -- license: GPL-3.0 License                                                                            -- #
